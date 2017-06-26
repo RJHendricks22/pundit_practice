ec8e936a1e7af3e050d215a11443670798dfc6b9
Scott Kaplan <scottkaplan.sk@gmail.com>
2017-06-26 13:10:44 -0400

    enum setup

--- qwikist/app/models/user.rb
+++ qwikist/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+
+  enum role: [:client, :trainer, :gym, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :client
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
